f718cbd7c07a7c4ed5a5946f8064063335d7ced4
Spencer Fornaciari <mail@spencerfornaciari.com>
2014-04-17 09:53:40 -0700

    Initial Commit

---
+++ SearchDisplay/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SearchDisplay
+//
+//  Created by Spencer Fornaciari on 4/17/14.
+//  Copyright (c) 2014 Spencer Fornaciari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SearchDisplay/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SearchDisplay
+//
+//  Created by Spencer Fornaciari on 4/17/14.
+//  Copyright (c) 2014 Spencer Fornaciari. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
